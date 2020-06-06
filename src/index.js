53ac030f6e5dc32b629c393d9185852209a1fd48
Aashay <aashayphirke5798@gmail.com>
2020-06-06 20:16:08 +0530

    update files

--- src/index.js
+++ src/index.js
@@ -12,17 +12,19 @@ import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import createSagaMiddleware from "redux-saga";
 import { watchAuth, watchBurgerBuilder, watchOrder } from "./store/sagas/";
+
 const composeEnhancers =
   process.env.NODE_ENV === "development"
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    : null || compose;
-
+    : compose;
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
   order: orderReducer,
   auth: authReducer,
 });
+
 const sagaMiddleware = createSagaMiddleware();
+
 const store = createStore(
   rootReducer,
   composeEnhancers(applyMiddleware(thunk, sagaMiddleware))
@@ -44,4 +46,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
